9001a118dba12fa54518105bb5de829675b62472
paridhiiyadav82-lang <paridhiiyadav82@gmail.com>
2026-07-17 10:50:20 +0530

    variables, input(), indexing, slicing,

--- day1.py
+++ day1.py
@@ -1,1 +1,24 @@
 print("Agrigate starts on July 15")
+message = "Hello"
+name=input("whats your name? ")
+print(message, name)
+quote_of_the_day='Marie Curie once said, "Have no fear of perfection: you\'ll never reach it" '
+print(quote_of_the_day)
+print("Now let's take a look at this poem")
+poem="""Two roads diverged in a wood, and I-
+I took the one less travelled by 
+And that has made all the difference"""
+print(poem)
+print("But how many characters were these three lines?")
+print(len(poem))
+# to calculate how many characters 
+print(poem[4])
+#to display a particular character from a text, here 4 = position of that character 
+print(poem[0:18])
+#to display a range of characters from a text
+print("lets see in this lower case")
+print(poem.lower())
+#to make things into lower case
+print(poem.count("Two"))
+
+
